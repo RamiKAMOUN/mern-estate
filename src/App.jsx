7a2f2b2e3f1d6ec64aa5262c364395658a29f28e
RamiKAMOUN <rami.kammoun12@gmail.com>
2023-10-07 11:59:04 +0100

    creat header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+import About from './pages/About'
+
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>
-      App
-    </h1>
+  return( 
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/sign-in" element={<SignIn />}/>
+      <Route path="/Sign-out" element={<SignOut/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+    
+  </BrowserRouter>
   )
 }
